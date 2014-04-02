a32e29d0b94ab3d9d8a1489d768f50310d4a1c99
Nathaniel Lash <lash2@illinois.edu>
2014-04-02 16:31:26 -0500

    Let's go

---
+++ scrape_courses.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+import credentials
+from listings import listings
+
+ 
+browser = webdriver.Firefox()
+browser.get('https://www-s.dmi.illinois.edu/course/crscrssearch.asp')
+
+user_id_field = browser.find_element_by_id('UserID')
+password_field = browser.find_element_by_id('Password')
+
+user_id_field.send_keys(credentials.my_netid)
+password_field.send_keys(credentials.my_password)
+
+
+button = browser.find_element_by_name('BTN_LOGIN')
+button.click()
+
+# at this point, you should be logged in!
+
+
+#here are the functions
+#this guy grabs the table and writes it out to a csv
+def grab_data():
+	#bootyfulsoups goes here
+	#writes out
+	
+	pass
+
+
+def process_option(i, el):
+	#this chooses the first element only
+	#button = browser.find_element_by_name('rptchoice')
+	#but we want the button labeled "Detail"
+	button = browser.find_elements_by_name('rptchoice')[1]
+
+	el = browser.find_element_by_name('crschoice2')
+	el = el.find_elements_by_tag_name('option')
+	el[i].click()
+	button.click()
+	grab_data()
+	browser.back() # and go back!
+
+
+for l in listings:
+	course_field = browser.find_element_by_name('rubrchoice')
+	course_field.send_keys(l)
+	button = browser.find_element_by_name('rptchoice')
+	button.click()
+
+	button = browser.find_element_by_name('rptchoice')
+	el = browser.find_element_by_name('crschoice2')
+	el = el.find_elements_by_tag_name('option')
+	for i in range(len(el)):
+		process_option(i, el)
+
+	browser.back() 	#go back again so we can input a new listing!
+
+
+''' here is a snippet of my first attempt at traversing the options, preserved for posterity
+for option in el.find_elements_by_tag_name('option'):
+	el = browser.find_element_by_name('crschoice2')
+	option.click()
+	button.click()
+	browser.back()
+this method does not work, because of the way selenium works. we can look more into this later.
+'''
+
+
+
+
